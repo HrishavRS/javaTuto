428b1ea4d79838a30af4fcc2447b3a4c2920a35e
Hrishav <hrisav_rs@gmail.com>
2021-12-30 14:18:02 +0545

    JavaStream 2nd commit

--- Steams/Steam.java
+++ Steams/Steam.java
@@ -8,11 +8,20 @@ import java.util.stream.*;
 
 class Streamss{
 	public static void main(String[] args) throws IOException{
-
+		//1. Integer Stream
 		IntStream
 			.range(1,10)
 			.forEach(System.out::print);
 		System.out.println();
+
+		//2.Integer Stream with skip
+		IntStream
+			.range(1,10)
+			.skip(5)
+			.forEach(x->System.out.print(x));
+		System.out.println();
+
+		//3.
 
 	}
 }
